6f2e102d00db297cd6ce5ef17a103bfea8e90d45
Alec Levin <alec@arlevin.org>
2021-03-21 22:44:17 -0400

    wire up join/create buttons and transition to shell brainstorming activity

--- app/src/main/java/org/arlevin/leancoffree/MainActivity.kt
+++ app/src/main/java/org/arlevin/leancoffree/MainActivity.kt
@@ -3,7 +3,13 @@ package org.arlevin.leancoffree
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +22,31 @@ class MainActivity : AppCompatActivity() {
     fun onJoinClick(view: View) {
         val intent = Intent(this, JoinSessionActivity::class.java).apply {}
         startActivity(intent)
+    }
+
+    fun onCreateClick(view: View) {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://leancoffree.com:8085/create-session"
+
+        val stringRequest = StringRequest(
+            Request.Method.POST, url,
+            Response.Listener<String> { response ->
+                val jsonResponse = JSONObject(response)
+                if (jsonResponse.getString("id").isNotBlank()) {
+                    val intent = Intent(this, BrainstormingActivity::class.java).apply {
+                        putExtra("id", jsonResponse.getString("id"))
+                        putExtra("status", "STARTED")
+                    }
+                    startActivity(intent)
+                }
+            },
+            Response.ErrorListener {
+                Toast.makeText(
+                    applicationContext, "An error occurred, please retry", Toast.LENGTH_SHORT
+                ).show()
+            }
+        )
+
+        queue.add(stringRequest)
     }
 }
