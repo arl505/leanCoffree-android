c8c7a3fc54897914620bd1d8ebc39f416c536c56
Alec Levin <alec@arlevin.org>
2021-03-24 18:56:47 -0400

    display votes

--- app/src/main/java/org/arlevin/leancoffree/session/BrainstormingAdapter.kt
+++ app/src/main/java/org/arlevin/leancoffree/session/BrainstormingAdapter.kt
@@ -23,13 +23,16 @@ class BrainstormingAdapter() : RecyclerView.Adapter<BrainstormingAdapter.Brainst
 
     override fun onBindViewHolder(holder: BrainstormingViewHolder, position: Int) {
         val currentItem = JSONObject(brainstormingList[position].toString())
-        holder.tv.text = currentItem.getString("text")
+        val votesText = "Votes: " + currentItem.getJSONArray("voters").length()
+        holder.topicTv.text = currentItem.getString("text").trim()
+        holder.votesTv.text = votesText
     }
 
     override fun getItemCount() = brainstormingList.size
 
     class BrainstormingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tv: TextView = itemView.findViewById<TextView>(R.id.brainstormingTopicTv)
+        val topicTv: TextView = itemView.findViewById(R.id.brainstormingTopicTv)
+        val votesTv: TextView = itemView.findViewById(R.id.brainstormingVotesTv)
     }
 
     fun updateBrainstormingList(activity: Activity, newList: ArrayList<JSONObject>) {
