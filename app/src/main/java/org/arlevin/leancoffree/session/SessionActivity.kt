8084db8306df7d0fadfbd826bec71b287bf6f785
Alec Levin <alec@arlevin.org>
2021-03-24 21:05:43 -0400

    appropriately display votes/unvote button according to voters list per topic and votes left

--- app/src/main/java/org/arlevin/leancoffree/session/SessionActivity.kt
+++ app/src/main/java/org/arlevin/leancoffree/session/SessionActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import okhttp3.OkHttpClient
 import org.arlevin.leancoffree.Constants
 import org.arlevin.leancoffree.R
+import org.json.JSONArray
 import org.json.JSONObject
 import ua.naiksoftware.stomp.StompClient
 
@@ -17,6 +18,7 @@ class SessionActivity : AppCompatActivity() {
         var username = ""
         var topics = JSONObject()
         var users = JSONObject()
+        var votesLeft = 3
     }
 
     private val stompClient: StompClient = StompClient(
@@ -43,6 +45,7 @@ class SessionActivity : AppCompatActivity() {
         { message ->
             topics = JSONObject(message.payload)
             BrainstormingFragment.notifyTopics(this, topics.getJSONArray("discussionBacklogTopics"))
+            tabulateVotesLeft(topics.getJSONArray("discussionBacklogTopics"))
         },
         {
             throw Exception("Unable to subscribe to topic")
@@ -59,5 +62,18 @@ class SessionActivity : AppCompatActivity() {
             replace(R.id.sessionFrame, BrainstormingFragment())
             commit()
         }
+    }
+
+    private fun tabulateVotesLeft(backlogTopics: JSONArray) {
+        var votesCast = 0
+        for (i in 0 until backlogTopics.length()) {
+            val voters: JSONArray = JSONObject(backlogTopics[i].toString()).getJSONArray("voters")
+            for (j in 0 until voters.length()) {
+                if (voters[j] == username) {
+                    votesCast += 1
+                }
+            }
+        }
+        votesLeft = 3 - votesCast
     }
 }
